5d5fa36273f203bd3702bad88b51470ddeda19cb
Quang Thiện <108052745+devlow03@users.noreply.github.com>
2023-10-04 16:08:30 +0700

    Update buoi1.sol

--- buoi1.sol
+++ buoi1.sol
@@ -2,7 +2,7 @@
 pragma solidity > 0.8.0;
 contract HouseManager{
     struct House{
-        uint id;
+        uint  id;
         string house_address;
         address owner;
 
@@ -15,7 +15,8 @@ contract HouseManager{
     
 
     function addHouse(string memory _address) public{
-        uint id = house.length;
+        
+        uint id = house.length > 0 ? house[house.length - 1].id + 1:0;
         house.push(House(id,_address,msg.sender));
         owners[id] = msg.sender;
     }
@@ -23,6 +24,7 @@ contract HouseManager{
     function deleteHouse(uint _id)public{
         require(owners[_id]==msg.sender,"You are not owner");
         delete house[_id];
+        delete  owners[_id];
     }
 
     function updateHouse(uint _id, string memory _address) public{
